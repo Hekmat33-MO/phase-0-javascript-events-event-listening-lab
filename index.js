bd7c03f63962256579d40b0f8b20e98e6f514152
Hekmat Mohammed <heka332020@gmail.com>
2023-01-02 18:19:57 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const field = document.getElementById("button");
+
 function addingEventListener() {
-}
+
+    field.addEventListener("click", function() {
+        alert("I was clicked!");
+    });
+};
